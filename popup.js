1e7618f46642401a87036152c3d902e7cb83c651
junjian_chen <junjian.chen@shaoke.com>
2022-09-19 13:01:41 +0800

    simplify code

--- popup.js
+++ popup.js
@@ -2,10 +2,7 @@ let input = document.getElementById('input')
 input.addEventListener('input', function(e) {
   let code = e.target.value.trim()
   if (!code) return
-  JsBarcode("#barcode", code, {
-    displayValue: false,
-    font: 'Tahoma'
-  });
+  JsBarcode("#barcode", code, { displayValue: false })
 })
 input.select()
 document.execCommand('paste')
