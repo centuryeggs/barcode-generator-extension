7664052efe9b125f9021bebbde6d680e649c80ee
centuryeggs <junjianch@gmail.com>
2025-12-14 00:06:44 +0800

    fix: display bug

--- popup.js
+++ popup.js
@@ -1,8 +1,35 @@
 let input = document.getElementById('input')
+let tip = document.getElementById('tip')
+let barcode = document.getElementById('barcode')
+
+function setEmptyTip() {
+  tip.style.color = 'black'
+  tip.textContent = 'Please enter a code'
+}
+
+if (!input.value.trim()) setEmptyTip()
+
 input.addEventListener('input', function(e) {
   let code = e.target.value.trim()
-  if (!code) return
-  JsBarcode("#barcode", code, { displayValue: false })
+  if (!code) {
+    setEmptyTip()
+    barcode.style.display = 'none'
+    return
+  }
+  try {
+    barcode.style.display = 'block'
+    window.JsBarcode("#barcode", code, { 
+      displayValue: false,
+      height: 140,
+      margin: 0
+    })
+    tip.textContent = ''
+  } catch (err) {
+    barcode.style.display = 'none'
+    tip.style.color = 'red'
+    tip.textContent = err
+  }
 })
+
 input.select()
 document.execCommand('paste')
